1eaa4033bf52ffb05ab62a6656e6f7eea9b61166
kosh <mirage.j@mail.com>
2024-02-26 18:46:05 +0300

    first commit yeah

---
+++ codewars.js
@@ -0,0 +1,165 @@
+// CONSTS ---------------------------------------------------------
+function getFee(isMember) {
+  return "The fee is " + (isMember ? "$2.00" : "$10.00");
+}
+// FUNCTIONS ---------------------------------------------------------
+
+function get_count(sentence) {
+  var counter = 0;
+  for (let i = 0; i < sentence.length; i++) {
+    if (["a", "e", "i", "o", "u"].includes(sentence[i])) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+function disemvowel(str) {
+  var newstr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (vovels.includes(str[i])) {
+    } else {
+      newstr = newstr + str[i];
+    }
+  }
+  return newstr;
+}
+
+function squareDigits(num) {
+  let split_num = String(num).split("");
+  for (let i = 0; i < split_num.length; i++) {
+    split_num[i] = String(Number(String(num).split("")[i]) ** 2);
+  }
+  return Number(split_num.join(""));
+}
+
+function descendingOrder(n) {
+  return Number(String(n).split("").sort().reverse().join(""));
+}
+
+function highAndLow(numbers) {
+  let max = Math.max(...numbers.split(" "));
+  let min = Math.min(...numbers.split(" "));
+  return `${max} ${min}`;
+}
+
+function highAndLow(numbers) {
+  numbers = numbers.split(" ");
+  return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
+
+// целочисленное деление. остаток. тернарный операторю индекс массива
+function getMiddle(s) {
+  return s.length % 2
+    ? s[Math.floor(s.length / 2)]
+    : s[s.length / 2 - 1] + s[s.length / 2];
+}
+
+// подстрока String.prototype.substring()
+function getMiddle(s) {
+  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+}
+
+//форматированная строка. цикл for if
+function accum(s) {
+  result = "";
+  for (let i = 0; i < s.length; i++) {
+    result += `${s.split("")[i].toUpperCase()}${s
+      .split("")
+      [i].repeat(i)
+      .toLowerCase()}`;
+    if (i + 1 != s.length) result += "-";
+  }
+  return result;
+}
+
+// разобрать функцию map
+function accum(s) {
+  return s
+    .split("")
+    .map((c, i) => c.toUpperCase() + c.toLowerCase().repeat(i))
+    .join("-");
+}
+
+// определить тип number or string
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  return l.filter((elem) => typeof elem === "number");
+}
+
+//оператор И, квадратный корень, деление нацело
+var isSquare = function (n) {
+  return n >= 0 && Math.pow(n, 0.5) % 1 == 0;
+};
+
+function isSquare_2(n) {
+  return Math.sqrt(n) % 1 === 0;
+}
+
+// сколько раз встречается буква в строке с помощтю split
+function XO(str) {
+  // конкретно в этом примере -1 не имеет значения для сравнения
+  return (
+    str.toLowerCase().split("o").length - 1 ===
+    str.toLowerCase().split("x").length - 1
+  );
+}
+
+// больше ли одного раза встречается буква в строке с помощтю split
+function isIsogram(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str.toLowerCase().split(str[i]).length - 1 > 1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// больше ли одного раза встречается буква в строке с помощтю set
+function isIsogram(str) {
+  return new Set(str.toUpperCase()).size == str.length;
+}
+
+String.prototype.toJadenCase_or = function () {
+  const word = "freecodecamp";
+
+  const firstLetter = word.charAt(0);
+
+  const firstLetterCap = firstLetter.toUpperCase();
+
+  const remainingLetters = word.slice(1);
+
+  const capitalizedWord = firstLetterCap + remainingLetters;
+};
+
+//Capitalize the string
+//странное требование через prototype this
+function toJadenCase(str) {
+  return str.split(' ').map((x) => x[0].toUpperCase()+x.slice(1)).join(' ')
+}
+
+String.prototype.toJadenCase = function () {
+  return this.split(' ').map((x) => x[0].toUpperCase()+x.slice(1)).join(' ')
+};
+
+
+
+// EXEC ----------------------------------------
+function dnaStrand(dna){
+  
+  pairs = {
+    'A': 'T',
+    'T': 'A',
+    'C': 'G',
+    'G': 'C'
+  }
+
+  // return ''.join([pairs[x] for x in dna])
+  return dna.split('').map((x) => pairs[x]).join('')
+
+}
+
+
+// EXEC ----------------------------------------------------------------
+
+console.log(dnaStrand("ATTGC"));
---
+++ codewars7.py
@@ -0,0 +1,149 @@
+def square_digits(num):
+    b = list(str(num))
+    result = []
+    for i in b:
+        result.append(str(int(i) ** 2))
+
+    c = int(''.join(result))
+    print(c)
+
+
+def square_digits_2(num):
+    a = int(''.join([str(int(x) ** 2) for x in str(num)]))
+    print(a)
+
+
+def disemvowel(string_):
+    asd = ''.join([l for l in string_ if l not in 'euioaEUIOA'])
+    print(asd)
+
+
+def get_count(sentence):
+    v = 0
+    for l in sentence:
+        if l in [a, e, i, o, u]:
+            v += 1
+    return v
+
+
+def descending_order(num):
+    lst = (sorted([int(x) for x in [*(str(num))]], reverse=True))
+    return int(''.join(str(x) for x in lst))
+
+
+def Descending_Order_2(num):
+    return int("".join(sorted(str(num), reverse=True)))
+
+
+def high_and_low(numbers):
+    lst = [int(x) for x in numbers.split()]
+    print(str(max(lst)), str(min(lst)))
+    return (str(max(lst)), str(min(lst)))
+
+
+def accum(st: str):  # st "abcd"
+    return ('-'.join([(x[1] * (x[0] + 1)).title() for x in enumerate(st)]))
+
+
+def get_middle(s):
+    return s[len(s) // 2] if len(s) % 2 else s[len(s) // 2 - 1:len(s) // 2 + 1]
+
+
+# filter_list([1,2,'a','b']) == [1,2]
+def filter_list(l):
+    'return a new list with the strings filtered out'
+    return [x for x in l if type(x) != str]
+
+
+def is_square(n):
+    if n == 0:
+        return True
+    return (n ** (0.5)).is_integer() if n > 0 else False
+
+
+def is_square_2(n):
+    # return n >= 0 and (n ** 0.5) % 1 == 0
+    return 2.0 % 1 == 0
+
+
+# diff count in a string  methods ------------------------------
+def is_isogram(string):
+    check_dict = {}
+    for l in string.lower():
+        if check_dict.get(l):
+            return False
+        else:
+            check_dict[l] = 1
+    return True
+
+
+def is_isogram_set(string):
+    return len(string) == len(set(string.lower()))
+
+
+def is_isogram_count(string):
+    string = string.lower()
+    for letter in string:
+        if string.count(letter) > 1: return False
+    return True
+
+
+def xo(s):
+    return s.lower().count('o') == s.lower().count('x')
+
+
+# Методы капитализации строки -------------------------------------------------
+def to_jaden_case(string):
+    return ' '.join([x[0].upper() + x[1:] for x in string.split()])
+
+
+def to_jaden_case_2(string):
+    return ' '.join(word.capitalize() for word in string.split())
+
+
+import string
+
+
+def to_jaden_case_3(sentence):
+    formatted = string.capwords(sentence, sep=None)
+    return formatted
+
+
+# замена символов в строке ------------------------------------------------------------
+# через map
+def DNA_strand(dna):
+    dna_dict = {
+        'A': 'T',
+        'T': 'A',
+        'C': 'G',
+        'G': 'C',
+
+    }
+
+    def changer(l):
+        for n, v in dna_dict.items():
+            if l == n:
+                return v
+
+    dna = ''.join(list(map(changer, dna)))
+    return dna
+
+
+# через генератор
+def DNA_strand_2(dna):
+    pairs = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+    return ''.join([pairs[x] for x in dna])
+
+
+# через хитрожопую функцию translate python 3.4
+import string
+def DNA_strand_3(dna):
+    return dna.translate(string.maketrans("ATCG","TAGC"))
+    # Python 3.4 solution || you don't need to import anything :)
+    # return dna.translate(str.maketrans("ATCG","TAGC"))
+    # import string работает странно поэтому требует допиливания
+
+# EXEC ----------------------------------------------------------
+
+
+print(DNA_strand_3("ATTGC"))
